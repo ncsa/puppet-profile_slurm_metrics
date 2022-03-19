3bf2afc060cd0147ee7681cb984819ab545081f9
Ben Sperry <bsper2@illinois.edu>
2022-03-18 19:41:06 -0500

    SVCPLAN-596 and SVCPLAN-612
    Add collection of Slurm metrics via telegraf
    Add management of Slurm firewalls

---
+++ manifests/scheduler/firewall.pp
@@ -0,0 +1,37 @@
+# @summary Setup firewall on slurm scheduler
+#
+# @param slurmctld_port
+#   Destination port that need to be open for the slurmctld service
+#
+# @param slurmdbd_port
+#   Destination port that need to be open for the slurmdbd service
+#
+# @param sources
+#   Array of CIDRs that need to be open for the slurmctld and slurmdbd service
+#
+# @example
+#   include profile_slurm::scheduler::firewall
+class profile_slurm::scheduler::firewall (
+  Integer        $slurmctld_port,
+  Integer        $slurmdbd_port,
+  Array[String]  $sources,
+){
+
+  $sources.each | $source |
+  {
+    firewall { "100 allow ${source} access to slurmctld":
+      proto  => 'tcp',
+      dport  => $slurmctld_port,
+      source => $source,
+      action => 'accept',
+    }
+
+    firewall { "100 allow ${source} access to slurmdbd":
+      proto  => 'tcp',
+      dport  => $slurmdbd_port,
+      source => $source,
+      action => 'accept',
+    }
+  }
+
+}
